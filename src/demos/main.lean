96bac7c11e92e5124f30f10a4af53f67b8ca1362
Chase Norman <c_@berkeley.edu>
2023-01-12 01:49:46 -0500

    computable

--- src/demos/main.lean
+++ src/demos/main.lean
@@ -13,6 +13,9 @@ import data.nat.basic
 import data.list.basic
 import data.list.range
 
+--import algebra.order.monoid.nat_cast
+import logic.equiv.fin
+
 open nat
 open list
 open function
@@ -24,60 +27,41 @@ open alk amk distinct
 open direct_amo
 open sinz_amo
 
+@[derive decidable_eq]
+structure graph := (n : ℕ) (p : 0 < n) (w : fin n → fin n → ℤ) --(a : anti_symmetric w)
 
+@[derive decidable_eq]
+structure pointed_graph := (g : graph) (v : fin g.n)
 
-structure graph := (V : Type) (f : fintype V) (i : inhabited V) (d : decidable_eq V) (w : V → V → ℤ) --(a : anti_symmetric w)
+instance pointed_graph_inhabited : inhabited pointed_graph := 
+  inhabited.mk ⟨⟨1, nat.zero_lt_one, λ x y, 0⟩ , 0⟩
 
+--  inhabited.mk ⟨⟨bool, bool.fintype, inhabited.mk ff, bool.decidable_eq, λ x y, 0⟩, ff⟩
 
-instance i {g : graph} : fintype g.V := g.f
+structure graph_embedding := (g₁ : graph) (g₂ : graph) (f : fin g₁.n → fin g₂.n) (x : fin g₂.n) (p : set.univ \ (set.range f) = {x})
 
-/-instance v_decidable_eq {g : graph} : decidable_eq g.V := g.d
-instance w_decidable_eq {g : graph} : decidable_eq (g.V → g.V → ℤ) := by apply_instance
---instance g_decidable_eq : decidable_eq graph := by tactic.mk_dec_eq_instance
-instance g_decidable_eq : decidable_eq graph := 
-begin 
-  intros a b,
-  cases a,
-  cases b,
-  simp,
-  exact and.decidable,
-end-/
+def simple {V : Type} (a b : V) : fin 2 → V := (fin_two_arrow_equiv V).inv_fun ⟨a, b⟩
 
-
-
-structure pointed_graph := (g : graph) (v : g.V)
-
-
-
-
-instance pointed_graph_inhabited : inhabited pointed_graph := inhabited.mk ⟨⟨bool, bool.fintype, inhabited.mk ff, bool.decidable_eq, λ x y, 0⟩, ff⟩
-
-structure graph_embedding := (g₁ : graph) (g₂ : graph) (f : g₁.V → g₂.V) (x : g₂.V) (p : set.univ \ (set.range f) = {x})
-
-def simple {V : Type} (a b : V) : bool → V
-| ff := a
-| tt := b
-
-def special1 (e : graph_embedding) (v : e.g₁.V) : graph := 
-  ⟨bool, bool.fintype, inhabited.mk ff, bool.decidable_eq, 
+def special1 (e : graph_embedding) (v : fin e.g₁.n) : graph := 
+  ⟨2, zero_lt_two, 
     (ite (e.g₂.w (e.f v) e.x > 0) 
       (λ a b, e.g₂.w (simple (e.f v) e.x a) (simple (e.f v) e.x b))
       (λ a b, e.g₂.w (simple (e.f v) e.x b) (simple (e.f v) e.x a)))⟩
 
-def special2 (e : graph_embedding) (v : e.g₁.V) : bool := 
-  (ite (e.g₂.w (e.f v) e.x > 0) ff tt)
+def special2 (e : graph_embedding) (v : fin e.g₁.n) : fin 2 := 
+  (ite (e.g₂.w (e.f v) e.x > 0) ⟨0, zero_lt_two⟩ ⟨1, one_lt_two⟩)
 
 def encoding (D : list graph) (E : list graph_embedding) : cnf pointed_graph := 
   -- at least one literal selected for each graph
-  (D.map (λ g: graph, ((fintype.elems (g.V)).to_list.map (λ v, Pos ⟨g, v⟩)))) ++ 
+  (D.map (λ g: graph, list.of_fn (λ v, Pos ⟨g, v⟩))) ++ 
   -- at most one literal selected for each graph
-  join (D.map (λ g: graph, direct_amo ((fintype.elems (g.V)).to_list.map (λ v, Pos ⟨g, v⟩)))) ++
+  join (D.map (λ g: graph, direct_amo (list.of_fn (λ v, Pos ⟨g, v⟩)))) ++
   -- binary gamma for each embedding
-  join (E.map (λ e, ((fintype.elems e.g₁.V).to_list.map (λ v, [Neg ⟨special1 e v, special2 e v⟩, Neg ⟨e.g₁, v⟩, Pos ⟨e.g₂, e.f v⟩])) ))
+  join (E.map (λ e, (list.of_fn (λ v, [Neg ⟨special1 e v, special2 e v⟩, Neg ⟨e.g₁, v⟩, Pos ⟨e.g₂, e.f v⟩])) ))
 
---#eval encoding nil nil
+#eval (encoding [⟨1, nat.zero_lt_one, λ x y, 0⟩] nil)
 
-def binary_gamma (E : list graph_embedding) (f : Π g : graph, g.V) := ∀ e : graph_embedding, e ∈ E → (f (special1 e (f e.g₁))) = (special2 e (f e.g₁)) → (f e.g₂ = e.f (f e.g₁))
+def binary_gamma (E : list graph_embedding) (f : Π g : graph, fin g.n) := ∀ e : graph_embedding, e ∈ E → (f (special1 e (f e.g₁))) = (special2 e (f e.g₁)) → (f e.g₂ = e.f (f e.g₁))
 
 lemma lemma1 {n : ℕ} (a : ¬ n = 1) (b : ¬ n ≥ 2) : n = 0 := by omega
 
@@ -87,16 +71,15 @@ begin
   rw eval_tt_iff_forall_clause_eval_tt,
   intros cl hcl,
   simp [encoding] at hcl,
-  rcases hcl with (⟨g, gd, h⟩ | ⟨g, gd, hcl⟩ | ⟨e, ee, v, p, h⟩),
+  rcases hcl with (⟨g, gd, h⟩ | ⟨g, gd, hcl⟩ | ⟨e, ee, h⟩),
   {
     subst h,
-    simp [eval_tt_iff_exists_literal_eval_tt],
+    simp [eval_tt_iff_exists_literal_eval_tt, list.mem_of_fn],
     use f g,
-    use i.complete (f g),
     simp [literal.eval],
   },
   {
-    by_cases (length ((fintype.elems (g.V)).to_list.map (λ v, Pos (pointed_graph.mk g v)))) ≥ 2,
+    by_cases (length (list.of_fn (λ v, Pos (pointed_graph.mk g v)))) ≥ 2,
     {
       rcases exists_double_flip_eq_of_mem h hcl with ⟨lit₁, lit₂, rfl⟩,
       rcases distinct_iff_mem.mpr hcl with ⟨i, j, hi, hj, hij, rfl, rfl⟩,
@@ -105,19 +88,11 @@ begin
       by_contradiction,
       simp [literal.eval] at h,
       cases h with h1 h2,
-      rw h2 at h1,
-      let test := (fintype.elems g.V).nodup_to_list,
-      unfold nodup at test,
-      rw list.pairwise_iff_nth_le at test,
-      rw list.length_map at hj,
-      rw list.length_map at hi,
-      specialize test i j hj hij,
-      change ((fintype.elems g.V).to_list.nth_le i hi ≠ (fintype.elems g.V).to_list.nth_le j hj) at test,
-      change ((fintype.elems g.V).to_list.nth_le j hj = (fintype.elems g.V).to_list.nth_le i hi) at h1,
-      exact test (eq.symm h1),
+      rw h1 at h2,
+      exact (eq.not_lt (fin.veq_of_eq h2)) hij,
     },
     let h2 := h,
-    by_cases (length ((fintype.elems (g.V)).to_list.map (λ v, Pos (pointed_graph.mk g v)))) = 1,
+    by_cases (length (list.of_fn (λ v, Pos (pointed_graph.mk g v)))) = 1,
     {
       rw list.length_eq_one at h,
       cases h with a h,
@@ -136,6 +111,9 @@ begin
     },
   },
   {
+    rw list.mem_of_fn at h,
+    rw set.mem_range at h,
+    cases h with y h,
     subst h,
     simp [eval_tt_iff_exists_literal_eval_tt],
     simp [literal.eval],
